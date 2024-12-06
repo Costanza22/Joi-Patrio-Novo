55c71ca1d500a170d3e91b7f8c2cc568bba878fb
Costanza Pasquotto Assef <81304267+Costanza22@users.noreply.github.com>
2024-12-06 12:47:51 -0300

    Update jest.config.js

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,6 +1,13 @@
 export default {
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+  },
   collectCoverage: true,
   coverageReporters: ['lcov', 'text-summary'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.js'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 };
