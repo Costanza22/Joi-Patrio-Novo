792a51d5c4f61160b2b00b675d60111b6800be64
Costanza Pasquotto Assef <81304267+Costanza22@users.noreply.github.com>
2024-12-07 18:20:25 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
 import cors from 'cors';
-import bcrypt from 'bcrypt.js';
+import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 const app = express();
