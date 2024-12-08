96576250af7e64df19d44e6405938e0c68959b87
Costanza22 <81304267+Costanza22@users.noreply.github.com>
2024-12-08 18:03:15 -0300

    permitindo todas as origens

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Configuração mais permissiva do CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://joinville-version.vercel.app/');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
